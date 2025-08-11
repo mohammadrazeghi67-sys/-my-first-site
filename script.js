92e899a2ef03c5edf8ffd316611f1daf44b96c05
mohammadrazeghi67-sys <mohammad.razeghi67@gmail.com>
2025-08-11 17:25:38 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("hello-btn");
+  const msg = document.getElementById("msg");
+
+  btn.addEventListener("click", () => {
+    const now = new Date().toLocaleTimeString();
+    msg.textContent = "سلام! الان ساعت " + now;
+  });
+});
 
